ead2e50eb5d575533f058053618dfb3645fc94e3
Нурсултан <nursultanerzhan9301@gmail.com>
2022-07-06 00:13:18 +0600

    TokenContextProvider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import {useToken} from './hooks/useToken';
-import {tokenContext} from './context/tokenContext';
 import {AuthContextProvider} from './context/authContext';
+import {TokenContextProvider} from './context/tokenContext';
 
 function App() {
-  const [token, delToken] = useToken('');
-
   return (
-    <tokenContext.Provider value={{token, delToken}}>
+    <TokenContextProvider>
       <AuthContextProvider>
         <Header/>
         <Main />
       </AuthContextProvider>
-    </tokenContext.Provider>
+    </TokenContextProvider>
   );
 }
 
