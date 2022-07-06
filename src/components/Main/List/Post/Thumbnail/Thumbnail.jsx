2960222ed82d1b10bdf1cbf4f9e1849de44f8977
Нурсултан <nursultanerzhan9301@gmail.com>
2022-07-06 14:20:36 +0600

    style changed

--- src/components/Main/List/Post/Thumbnail/Thumbnail.jsx
+++ src/components/Main/List/Post/Thumbnail/Thumbnail.jsx
@@ -2,13 +2,10 @@ import style from './Thumbnail.module.css';
 import notphoto from './img/notphoto.jpg';
 import PropTypes from 'prop-types';
 
-export const Thumbnail = ({thumbnail}) => {
-  console.log(style);
-  return (
-    <img className={style.img} src={thumbnail.length > 5 ? thumbnail :
+export const Thumbnail = ({thumbnail}) => (
+  <img className={style.img} src={thumbnail.length > 5 ? thumbnail :
       notphoto} alt={thumbnail} />
-  );
-};
+);
 
 Thumbnail.propTypes = {
   thumbnail: PropTypes.string,
