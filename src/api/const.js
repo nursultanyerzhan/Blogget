7e98d455958f6a62cc641df84f3ad26b8e42728e
Нурсултан <nursultanerzhan9301@gmail.com>
2022-07-19 21:33:28 +0600

    vercel

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const CLIENT_ID = 'rU_6BrfF5cTfnFYCTHr13Q';
 export const RESPONSE_TYPE = 'token';
 export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'https://wondrous-begonia-70201d.netlify.app/auth';
+export const REDIRECT_URI = 'https://blogget-six.vercel.app/auth';
 export const SCOPE = 'identity submit read';
